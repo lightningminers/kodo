9296d61d5941ee72deb6eacc5a25775e5f17a61f
icepy <xiangwenwe@foxmail.com>
2016-05-18 14:29:15 +0800

    给dialog的buttons添加class属性

--- dist/kodo.js
+++ dist/kodo.js
@@ -594,9 +594,9 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ },
 /* 18 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 	
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
@@ -604,17 +604,29 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 	
+	var _classnames = __webpack_require__(12);
+	
+	var _classnames2 = _interopRequireDefault(_classnames);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+	
 	function renderButtons(buttons) {
 	  return buttons.map(function (action, idx) {
-	    var label = action.label;
+	    var _classNames;
 	
 	    var type = action.type || 'plain';
+	    var label = action.label;
+	    var className = action.className;
+	
+	    var css = (0, _classnames2.default)((_classNames = {}, _defineProperty(_classNames, 'kodo_buttons_' + type, type), _defineProperty(_classNames, className, className), _classNames));
 	    return React.createElement(
-	      "a",
+	      'a',
 	      _extends({}, action, {
-	        type: type,
 	        key: idx,
-	        href: "javascript:;"
+	        className: css,
+	        href: 'javascript:;'
 	      }),
 	      label
 	    );
--- src/components/dialog/buttons.js
+++ src/components/dialog/buttons.js
@@ -1,12 +1,18 @@
+import classNames from 'classnames';
+
 function renderButtons (buttons){
   return buttons.map(( action, idx )=>{
-    const { label } = action;
     let type = action.type || 'plain';
+    const { label, className } = action;
+    const css = classNames({
+      ['kodo_buttons_'+type]: type,
+      [className]:className
+    });
     return (
       <a
         {...action}
-        type={ type }
         key={ idx }
+        className={ css }
         href="javascript:;"
       >
         { label }
